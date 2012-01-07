4fc26cc797a9c3f8d389f7ec1d9b87a509a97516
Brad Graham <blg002@gmail.com>
2012-01-07 16:59:31 -0500

    front-end assignment of categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,7 +8,24 @@ class Article < ActiveRecord::Base
   has_many   :categorizations, dependent: :destroy
   has_many   :categories, through: :categorizations
   
+  attr_writer :category_names
+  after_save :assign_categories
+  
+  def category_names
+    @category_names ||= categories.map(&:name).join(', ')
+  end
+  
   def to_param
     "#{id}-#{title.parameterize}"
+  end
+  
+  private
+  
+  def assign_categories
+    if @category_names
+      self.categories = @category_names.split(', ').map do |name|
+        Category.find_or_create_by_name(name)
+      end
+    end
   end
 end
