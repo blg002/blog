04498a9881b35f5a914c9e61f74bce56e1c9e991
Brad Graham <blg002@gmail.com>
2012-01-15 23:55:08 -0500

    beginning of tests

---
+++ test/unit/article_test.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class ArticleTest < ActiveSupport::TestCase
+  
+  def setup
+    @article = articles(:one)
+  end
+  
+  test "create article without requirements" do
+    article = Article.new
+    assert !article.save, "Saved an article without any validations"
+  end
+  
+  # test "validations" do
+  #   assert(@article.valid?)
+  #   
+  #   @article.title = nil
+  #   assert(!@article.valid?)
+  #   
+  #   @article.title = "A title much longer than 140 character!! A title much longer than 140 character!! A title much longer than 140 character!! A title much longer than 140 character!!"
+  #   assert(!@article.valid?)
+  #   
+  #   @article.author_id = nil
+  #   assert(!@article.valid?)
+  #   
+  #   @article.body = nil
+  #   assert(!@article.valid?)
+  # end
+  
+end
