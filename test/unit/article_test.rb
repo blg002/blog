4cd7f9aa958744739d8cd9962e05bc107285a96d
Brad Graham <blg002@gmail.com>
2012-01-27 00:13:18 -0500

    some article model tests

--- test/unit/article_test.rb
+++ test/unit/article_test.rb
@@ -11,20 +11,21 @@ class ArticleTest < ActiveSupport::TestCase
     assert !article.save, "Saved an article without any validations"
   end
   
-  # test "validations" do
-  #   assert(@article.valid?)
-  #   
-  #   @article.title = nil
-  #   assert(!@article.valid?)
-  #   
-  #   @article.title = "A title much longer than 140 character!! A title much longer than 140 character!! A title much longer than 140 character!! A title much longer than 140 character!!"
-  #   assert(!@article.valid?)
-  #   
-  #   @article.author_id = nil
-  #   assert(!@article.valid?)
-  #   
-  #   @article.body = nil
-  #   assert(!@article.valid?)
-  # end
+  test "validations" do
+    assert(@article.valid?, "Article not passing validations")
+    
+    @article.title = nil
+    assert(!@article.valid?, "Title should be required")
+    
+    @article.author_id = nil
+    assert(!@article.valid?, "Author should be passing")
+    
+    @article.body = nil
+    assert(!@article.valid?, "Body should be required")
+  end
+
+  test "article to param" do
+    assert_equal "980190962-awesome-title", @article.to_param
+  end
   
 end
